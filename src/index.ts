e4ab4232bf315aeeaac478428ef38d832a6a73a6
mmccanless-sec <156618209+mmccanless-sec@users.noreply.github.com>
2024-02-05 19:03:17 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ export * from './nag-rules';
 export * from './nag-suppressions';
 export * from './packs/aws-solutions';
 export * from './packs/hipaa-security';
+export * from './packs/matt-checks'
 export * from './packs/nist-800-53-r4';
 export * from './packs/nist-800-53-r5';
 export * from './packs/pci-dss-321';
